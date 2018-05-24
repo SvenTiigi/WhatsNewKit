9f4a327cea462e08a98d688d7b9ded86f0261186
Sven Tiigi <sven.tiigi@gmail.com>
2018-05-24 14:49:57 +0200

    Added InMemoryWhatsNewVersionStore

---
+++ Sources/Store/InMemoryWhatsNewVersionStore.swift
@@ -0,0 +1,58 @@
+//
+//  InMemoryWhatsNewVersionStore.swift
+//  WhatsNewKit-iOS
+//
+//  Created by Sven Tiigi on 24.05.18.
+//  Copyright © 2018 WhatsNewKit. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - InMemoryWhatsNewVersionStore
+
+/// The InMemoryWhatsNewVersionStore
+public class InMemoryWhatsNewVersionStore {
+    
+    /// The Versions
+    private var versions: [WhatsNew.Version]
+    
+    /// Default initializer
+    public init() {
+        // Initialize Version Array
+        self.versions = .init()
+    }
+    
+    /// Clear all stored Versions
+    public func clearVersions() {
+        self.versions.removeAll()
+    }
+    
+}
+
+// MARK: - WriteableWhatsNewVersionStore
+
+extension InMemoryWhatsNewVersionStore: WriteableWhatsNewVersionStore {
+    
+    /// Set Version
+    ///
+    /// - Parameter version: The Version
+    public func set(version: WhatsNew.Version) {
+        // Append Version
+        self.versions.append(version)
+    }
+    
+}
+
+// MARK: - ReadableWhatsNewVersionStore
+
+extension InMemoryWhatsNewVersionStore: ReadableWhatsNewVersionStore {
+    
+    /// Has Version
+    ///
+    /// - Parameter version: The Version
+    /// - Returns: Bool if Version has been presented
+    public func has(version: WhatsNew.Version) -> Bool {
+        return self.versions.contains(version)
+    }
+    
+}
