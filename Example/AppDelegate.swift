5e12439bed98739613c38562c36c3ae2c6d7bab1
Sven Tiigi <sven.tiigi@gmail.com>
2018-09-18 10:55:12 +0200

    Migrated Test and Example Target to Swift 4.2

--- Example/AppDelegate.swift
+++ Example/AppDelegate.swift
@@ -19,12 +19,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let navigationController = UINavigationController(rootViewController: ExampleViewController())
         navigationController.navigationBar.prefersLargeTitles = true
         navigationController.navigationBar.largeTitleTextAttributes = [
-            NSAttributedStringKey.foregroundColor: UIColor.main
+            .foregroundColor: UIColor.main
         ]
         navigationController.view.backgroundColor = .white
         navigationController.navigationBar.tintColor = .main
         navigationController.navigationBar.titleTextAttributes = [
-            NSAttributedStringKey.foregroundColor: UIColor.main
+            .foregroundColor: UIColor.main
         ]
         return navigationController
     }()
@@ -36,7 +36,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     ///   - launchOptions: The LaunchOptions
     /// - Returns: Boolean
     func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Initialize Window
         self.window = UIWindow(frame: UIScreen.main.bounds)
         // Set root viewcontroller
