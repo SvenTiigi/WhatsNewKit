0620dbd0b32570f0c1253a6ae69af1301855d9e1
Sven Tiigi <sven.tiigi@gmail.com>
2019-02-02 10:24:51 +0100

    Added NSLayoutConstraint Extension

---
+++ Sources/Extensions/NSLayoutConstraint+Activate.swift
@@ -0,0 +1,79 @@
+//
+//  NSLayoutConstraint+Activate.swift
+//  WhatsNewKit-Example
+//
+//  Created by Sven Tiigi on 02.02.19.
+//  Copyright © 2019 WhatsNewKit. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - NSLayoutConstraint+Activate
+
+extension NSLayoutConstraint {
+    
+    /// Activate on View
+    ///
+    /// - Parameters:
+    ///   - view: The View
+    ///   - constraints: The Constraints
+    static func activate(on view: UIView, _ constraints: [NSLayoutConstraint]) {
+        view.translatesAutoresizingMaskIntoConstraints = false
+        self.activate(constraints)
+    }
+    
+}
+
+// MARK: - Anchor
+
+/// The Anchor Protocol
+protocol Anchor {
+    
+    /// The Top Anchor
+    var topAnchor: NSLayoutYAxisAnchor { get }
+    
+    /// The Bottom Anchor
+    var bottomAnchor: NSLayoutYAxisAnchor { get }
+    
+    /// The Leading Anchor
+    var leadingAnchor: NSLayoutXAxisAnchor { get }
+    
+    /// The Trailing Anchor
+    var trailingAnchor: NSLayoutXAxisAnchor { get }
+    
+    /// The Width Anchor
+    var widthAnchor: NSLayoutDimension { get }
+    
+    /// The Height Anchor
+    var heightAnchor: NSLayoutDimension { get }
+    
+    /// The center x anchor
+    var centerXAnchor: NSLayoutXAxisAnchor { get }
+    
+    /// The center y anchor
+    var centerYAnchor: NSLayoutYAxisAnchor { get }
+    
+}
+
+// MARK: - UIView+Anchor
+
+extension UIView: Anchor {}
+
+// MARK: - UILayoutGuide+Anchor
+
+extension UILayoutGuide: Anchor {}
+
+// MARK: - UIViewController+Anchor
+
+extension UIViewController {
+    
+    /// The Anchor
+    var anchor: Anchor {
+        if #available(iOS 11.0, *) {
+            return self.view.safeAreaLayoutGuide
+        } else {
+            return self.view
+        }
+    }
+    
+}
