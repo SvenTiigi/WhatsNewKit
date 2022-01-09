d67049d261ecd70563ca5850ae6895a75dbd592a
Andrew Bennet <me@andrewbennet.com>
2022-01-09 20:22:44 +0000

    Use `fixedSize(horizontal: false, vertical: true)`

--- Sources/View/WhatsNewView.swift
+++ Sources/View/WhatsNewView.swift
@@ -129,7 +129,7 @@ private extension WhatsNewView {
         )
         .font(.largeTitle.bold())
         .multilineTextAlignment(.center)
-        .fixedSize()
+        .fixedSize(horizontal: false, vertical: true)
     }
     
 }
@@ -159,13 +159,13 @@ private extension WhatsNewView {
                 )
                 .font(.subheadline.weight(.semibold))
                 .foregroundColor(.primary)
-                .fixedSize()
+                .fixedSize(horizontal: false, vertical: true)
                 Text(
                     whatsNewText: feature.subtitle
                 )
                 .font(.subheadline)
                 .foregroundColor(.secondary)
-                .fixedSize()
+                .fixedSize(horizontal: false, vertical: true)
             }
             .multilineTextAlignment(.leading)
         }
